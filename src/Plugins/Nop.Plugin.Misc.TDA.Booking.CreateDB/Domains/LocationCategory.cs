e6c79950d60bfe4ab4ec4fd4f858f2e1ebacb778
Quang <hnql1991@gmail.com>
2021-05-18 22:43:46 +0700

    Clean up

--- src/Plugins/Nop.Plugin.Misc.TDA.Booking.CreateDB/Domains/LocationCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Core;
-
-namespace Nop.Plugin.Misc.TDA.Booking.CreateDB.Domains
-{
-    public class LocationCategory : BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
--- src/Plugins/Nop.Plugin.Misc.TDA.Booking.CreateDB/Domains/RoomType.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Core;
-
-namespace Nop.Plugin.Misc.TDA.Booking.CreateDB.Domains
-{
-    public partial class RoomType : BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
