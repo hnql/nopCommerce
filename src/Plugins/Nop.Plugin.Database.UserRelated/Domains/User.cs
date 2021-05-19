30283ca929a59e7d0ba050d8c9df12b1e842c045
Tran Minh Tu <tu.tran@wiredbeans.co.jp>
2021-05-19 16:39:53 +0700

    Create plugin for user and related tables.

---
+++ src/Plugins/Nop.Plugin.Database.UserRelated/Domains/User.cs
@@ -0,0 +1,17 @@
+﻿using Nop.Core;
+
+namespace Nop.Plugin.Database.UserRelated.Domains
+{
+    class User : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Details { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
