d39efddb20eb33d29f57e0c62f0c0b52ef36660c
Quang <hnql1991@gmail.com>
2021-06-01 22:51:42 +0700

    Add some migration, clean up migration

---
+++ src/Plugins/Nop.Plugin.Booking.DB/Migrations/AddLanguage/DataMigration.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using FluentMigrator;
+using Nop.Core.Domain.Localization;
+using Nop.Data;
+using Nop.Data.Migrations;
+
+namespace Nop.Plugin.Booking.DB.Migrations.AddLanguage
+{
+    [SkipMigrationOnUpdate] // If you want to re-install plugin, please remove [SkipMigrationOnUpdate]
+    [NopMigration("2021/06/01 22:31:01:1234567", "Add data for language table")]
+    public class DataMigration : Migration
+    {
+        private readonly INopDataProvider _dataProvider;
+
+        public DataMigration(INopDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        /// <summary>
+        /// Collect the UP migration expressions
+        /// </summary>
+        public override void Up()
+        {
+            #region Insert data to Language table
+
+            var languageTable = _dataProvider.GetTable<Language>();
+            if (!languageTable.Any(lt => string.Compare(lt.Name, "VI", true) == 0))
+            {
+                _dataProvider.InsertEntity(
+                    new Language
+                    {
+                        Name = "VI",
+                        LanguageCulture = "vi-VN",
+                        UniqueSeoCode = "vi",
+                        FlagImageFileName = "vn.png",
+                        Rtl = false,
+                        DefaultCurrencyId = 0,
+                        Published = true,
+                        DisplayOrder = 2
+                    }
+                );
+            }
+
+            #endregion
+        }
+
+        public override void Down()
+        {
+            //var languageTable = _dataProvider.GetTable<Language>();
+            //var languageRecords = languageTable.Where(lt => lt.Name == "VI");
+            //foreach (var languageRecord in languageRecords)
+            //{
+            //    _dataProvider.DeleteEntityAsync(languageRecord);
+            //}
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Booking.DB/Migrations/AddLocaleStringResource/DataMigration.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+using FluentMigrator;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Localization;
+using Nop.Data;
+using Nop.Data.Migrations;
+
+namespace Nop.Plugin.Booking.DB.Migrations.AddLocaleStringResource
+{
+    [SkipMigrationOnUpdate] // If you want to re-install plugin, please remove [SkipMigrationOnUpdate]
+    [NopMigration("2021/06/01 21:31:01:1234567", "Add data for locale string resource table")]
+    public class DataMigration : Migration
+    {
+        private readonly INopDataProvider _dataProvider;
+
+        public DataMigration(INopDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        /// <summary>
+        /// Collect the UP migration expressions
+        /// </summary>
+        public override void Up()
+        {
+            #region Insert data to LocaleStringResource table
+
+            var localeStringResourceTable = _dataProvider.GetTable<LocaleStringResource>();
+            int englishId = 1;
+            int vietnameseId = 2;
+            if (!localeStringResourceTable.Any(lsrt => string.Compare(lsrt.ResourceName, "search.enterdates", true) == 0 && lsrt.LanguageId == englishId))
+            {
+                _dataProvider.InsertEntity(
+                    new LocaleStringResource
+                    {
+                        ResourceName = "search.enterdates",
+                        ResourceValue = "Dates",
+                        LanguageId = englishId
+                    }
+                );
+            }
+
+            if (!localeStringResourceTable.Any(lsrt => string.Compare(lsrt.ResourceName, "search.enterdates", true) == 0 && lsrt.LanguageId == vietnameseId))
+            {
+                _dataProvider.InsertEntity(
+                    new LocaleStringResource
+                    {
+                        ResourceName = "search.enterdates",
+                        ResourceValue = "Ngày",
+                        LanguageId = vietnameseId
+                    }
+                );
+            }
+
+            if (!localeStringResourceTable.Any(lsrt => string.Compare(lsrt.ResourceName, "search.enterguests", true) == 0 && lsrt.LanguageId == englishId))
+            {
+                _dataProvider.InsertEntity(
+                    new LocaleStringResource
+                    {
+                        ResourceName = "search.enterguests",
+                        ResourceValue = "Guests",
+                        LanguageId = englishId
+                    }
+                );
+            }
+
+            if (!localeStringResourceTable.Any(lsrt => string.Compare(lsrt.ResourceName, "search.enterguests", true) == 0 && lsrt.LanguageId == vietnameseId))
+            {
+                _dataProvider.InsertEntity(
+                    new LocaleStringResource
+                    {
+                        ResourceName = "search.enterguests",
+                        ResourceValue = "Khách",
+                        LanguageId = vietnameseId
+                    }
+                );
+            }
+
+            #endregion
+        }
+
+        public override void Down()
+        {
+            //var localeStringResourceTable = _dataProvider.GetTable<LocaleStringResource>();
+            //var localeStringResourceRecords = localeStringResourceTable.Where(pat => pat.ResourceName == "search.enterdates" || pat.ResourceName == "search.enterguests");
+            //foreach (var localeStringResourceRecord in localeStringResourceRecords)
+            //{
+            //    _dataProvider.DeleteEntityAsync(localeStringResourceRecord);
+            //}
+        }
+    }
+}
