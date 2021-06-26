52fbb35428dac10353e4a3d86e12a56d4c87f0ae
ThanhLai <thanhpo1212@gmail.com>
2021-06-26 20:07:59 +0700

    Initial

--- src/Plugins/Nop.Plugin.Booking.Main/Models/DestinationModel.cs
+++ src/Plugins/Nop.Plugin.Booking.Main/Models/DestinationModel.cs
@@ -1,80 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using Nop.Core.Domain.Media;
-using Nop.Web.Framework.Models;
+﻿using Nop.Web.Framework.Models;
 
 namespace Nop.Plugin.Booking.Main.Models
 {
     /// <summary>
     /// Represents a price range model
     /// </summary>
-    public partial record DestinationModel : BaseNopModel
+    public partial record DestinationModel : BaseNopModel 
     {
-        public struct Destination
+
+        public string VendorType { get; set; }
+        public string PictureUrl { get; set; }
+        public string StateProvince { get; set; }
+        public string Excerpt { get; set; }
+
+        public DestinationModel()
         {
-            public string Href;
-            public string ImagePath;
-            public string Title;
-            public string Excerpt;
-        }
-
-        #region Properties
-
-
-        public IList<Destination> Destinations { get; set; }
-
-        #endregion
-
-        public DestinationModel() {
-            Destinations = new List<Destination>();
-            Destinations.Add(new Destination { 
-                Href = "vung-tau-biet-thu-ho-boi-618", 
-                ImagePath = "apartment_2_1614588617.jpg",
-                Title = "Vũng Tàu Biệt thự hồ bơi",
-                Excerpt = "Những căn biệt thự có hồ bơi dành cho kỳ nghỉ của bạn tại Vũng Tàu"
-            });
-            Destinations.Add(new Destination
-            {
-                Href = "vi-vu-ngoai-thanh-ha-noi-619",
-                ImagePath = "apartment_1_1614588454.jpg",
-                Title = "Vi vu ngoại thành Hà Nội",
-                Excerpt = "Trải nghiệm không gian thoáng đãng cho chuyến đi ngay gần Hà Nội"
-            });
-            Destinations.Add(new Destination
-            {
-                Href = "ha-noi-noi-thanh-lang-man-620",
-                ImagePath = "apartment_1_1614660728.jpg",
-                Title = "Hà Nội nội thành lãng mạn",
-                Excerpt = "Không gian lãng mạn dành cho cặp đôi tại trung tâm Hà Nội"
-            });
-            Destinations.Add(new Destination
-            {
-                Href = "sai-gon-can-la-co-ngay-621",
-                ImagePath = "apartment_2_1615794965.jpg",
-                Title = "Sài Gòn cần là có ngay",
-                Excerpt = "Những căn homestay có 01 phòng ngủ tại Sài Gòn có thể đặt nhanh chóng"
-            });
-            Destinations.Add(new Destination
-            {
-                Href = "be-boi-bbq-468",
-                ImagePath = "apartment_1_1584606781.jpg",
-                Title = "Bể bơi & BBQ",
-                Excerpt = "Trải nghiệm đẳng cấp tại những căn homestay có bể bơi đẹp và khu vực BBQ ấm cúng."
-            });
-            Destinations.Add(new Destination
-            {
-                Href = "sieu-giam-gia-471",
-                ImagePath = "apartment_2_1584606872.jpg",
-                Title = "Siêu giảm giá!",
-                Excerpt = "Top chỗ ở giảm giá đến 50% từ các chủ nhà thân thiện trên Luxstay."
-            });
-            Destinations.Add(new Destination
-            {
-                Href = "gan-trung-tam-314",
-                ImagePath = "apartment_10_1584602562.jpg",
-                Title = "Gần Trung tâm!",
-                Excerpt = "Dễ dàng di chuyển khắp nơi với top chỗ ở khu vực trung tâm thành phố Hồ Chí Minh"
-            });
+            PictureUrl = "";
+            StateProvince = "Ha Noi";
         }
     }
 }
---
+++ src/Plugins/Nop.Plugin.Booking.Main/Services/DestinationService.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Common;
+using Nop.Core.Domain.Directory;
+using Nop.Core.Domain.Media;
+using Nop.Core.Domain.Vendors;
+using Nop.Data;
+using Nop.Plugin.Booking.Main.Models;
+using Nop.Services.Media;
+
+namespace Nop.Plugin.Booking.Main.Services
+{
+    public partial class DestinationService : IDestinationService
+    {
+
+        #region Fields
+
+        private readonly IRepository<Address> _addressRepository;
+        private readonly IRepository<StateProvince> _stateProvinceRepository;
+        private readonly IRepository<Vendor> _vendorRepository;
+        private readonly IRepository<Product> _productRepository;
+        private readonly IRepository<Picture> _pictureRepository;
+        private readonly IPictureService _pictureService;
+
+        #endregion
+
+        #region Ctor
+
+        public DestinationService(IRepository<Address> addressRepository,
+            IRepository<StateProvince> stateProvinceRepository,
+            IRepository<Vendor> vendorRepository,
+            IRepository<Product> productRepository,
+            IRepository<Picture> pictureRepository,
+            IPictureService pictureService)
+        {
+            _addressRepository = addressRepository;
+            _stateProvinceRepository = stateProvinceRepository;
+            _vendorRepository = vendorRepository;
+            _productRepository = productRepository;
+            _pictureRepository = pictureRepository;
+            _pictureService = pictureService;
+        }
+
+        #endregion
+
+        public virtual async Task<IList<DestinationModel>> GetDestinationAsync()
+        {
+            var models = new List<DestinationModel>();
+            var destinations = await (from v in _vendorRepository.Table
+                                   from a in _addressRepository.Table.Where(a => a.Id == v.AddressId).DefaultIfEmpty()
+                                   from sp in _stateProvinceRepository.Table.Where(sp => sp.Id == a.StateProvinceId).DefaultIfEmpty()
+                                   where !v.Deleted && v.Active
+                                   group new { sp, v } by new { sp.Name, v.PictureId, v.Description } into g
+                                   select new { StateProvince = g.Key.Name, PictureId = g.Key.PictureId, Description = g.Key.Description }
+                                   ).Distinct().ToListAsync();
+
+
+            foreach (var destination in destinations)
+            {
+                var model = new DestinationModel
+                {
+                    StateProvince = destination.StateProvince,
+                    PictureUrl = await _pictureService.GetPictureUrlAsync(destination.PictureId, showDefaultPicture: false) ?? "",
+                    Excerpt = destination.Description
+                };
+                models.Add(model);
+            }
+
+            return models;
+        }
+    }
+}
